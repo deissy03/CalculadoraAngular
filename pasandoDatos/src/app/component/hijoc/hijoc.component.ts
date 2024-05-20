9e703377379f19dcbb9adf72d713bf1309127cb0
deisy03 <desiyesquivia03@gmail.com>
2024-05-20 14:18:52 -0600

    pasando datos de hijo a padre

--- pasandoDatos/src/app/component/hijoc/hijoc.component.ts
+++ pasandoDatos/src/app/component/hijoc/hijoc.component.ts
@@ -8,19 +8,19 @@ import { Component,Input, Output,EventEmitter } from '@angular/core';
   styleUrl: './hijoc.component.css'
 })
 export class HijocComponent {
-  @Input() contadorPadre: number = 0; // input define una propiedad de entrada a la cual la llame contadorPadre y le indicamos q es de tipo numerico y le asignamos un valor 0
+  //@Input() contadorPadre: number = 0; // input define una propiedad de entrada a la cual la llame contadorPadre y le indicamos q es de tipo numerico y le asignamos un valor 0
   contador: number = 0;// hay declaramos una propiedad llamada contador de tipo numero empezando por 0, esta propiedad es independiente del input y se usa dentro de la clase hijo component
 
-  @Output() contadorHijoChange = new EventEmitter<number>();// aqui el output se crea una clase que nos define una propiedad de salida llamada contadorHijoChange, usamos una clase llamada EventEmitter que viene de angular lo que permite enviar datos del componente principal
+  @Output() contadorHijo = new EventEmitter<number>();// aqui el output se crea una clase que nos define una propiedad de salida llamada contadorHijo, usamos una clase llamada EventEmitter que viene de angular lo que permite enviar datos del componente principal
 //aqui creo dos funciones sumar y restar luego emito el valor que recibi a la variable contador 
   sumar(){ 
     this.contador++; 
-    this.contadorHijoChange.emit(this.contador);
+    this.contadorHijo.emit(this.contador);
   }
 
   restar(){
     this.contador--;
-    this.contadorHijoChange.emit(this.contador);
+    this.contadorHijo.emit(this.contador);
   }
 
   }
